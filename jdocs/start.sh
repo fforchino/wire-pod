f1422db28c881ea307f108b5b3fa273683134039
Filippo Forchino <filippo.forchino@gmail.com>
2022-11-25 08:40:11 +0100

    WIP

---
+++ jdocs/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root. sudo ./start.sh"
+  exit 1
+fi
+
+if [[ -d ./jdocs ]]; then
+   cd jdocs 
+fi
+
+#if [[ ! -f ./source.sh ]]; then
+#  echo "You need to make a source.sh file. This can be done with the setup.sh script."
+#  exit 0
+#fi
+
+#source source.sh
+
+/usr/local/go/bin/go run cmd/main.go
