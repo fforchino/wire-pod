c765f330124dc37ba3ef3d080645418b347ed0ad
frcfppud <filippo.forchino@gmail.com>
2023-04-21 21:10:53 +0200

    wip

--- vector-cloud/test/main.go
+++ vector-cloud/test/main.go
@@ -8,27 +8,28 @@ import (
 	"time"
 )
 
+var cloudSock ipc.Conn
+var testSender voice.IPCMsgSender
+
 func TriggerWakeWord() {
-	var cloudSock ipc.Conn
-
-	log.Println("Creating Test Client Socket to send messages to vic-cloud")
-	cloudSock = getSocketWithRetry(ipc.GetSocketPath("cp_test"), "cp_test_client")
-	defer cloudSock.Close()
-	log.Println("Socket created")
-
 	location_currentzone, _ := time.LoadLocation("Europe/Paris")
 	log.Println("Triggering hotword: " + location_currentzone.String())
 	hw := cloud.Hotword{Mode: cloud.StreamType_Normal, Locale: "en-US", Timezone: location_currentzone.String(), NoLogging: false}
 	message := cloud.NewMessageWithHotword(&hw)
 
-	log.Println("Creating sender")
-	testSender := voice.IPCMsgSender{Conn: cloudSock}
 	log.Println("Sending message")
 	testSender.Send(message)
 	log.Println("DONE")
 }
 
 func main() {
+	log.Println("Creating Test Client Socket to send messages to vic-cloud")
+	cloudSock = getSocketWithRetry(ipc.GetSocketPath("cp_test"), "cp_test_client")
+	defer cloudSock.Close()
+	log.Println("Socket created")
+	log.Println("Creating sender")
+	testSender = voice.IPCMsgSender{Conn: cloudSock}
+
 	log.Println("Now triggering wake word")
 	TriggerWakeWord()
 }
